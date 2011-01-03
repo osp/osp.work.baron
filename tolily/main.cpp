2198d40ae61e0ff04448ffddc0e4ad57da534914
Pierre M <pierremarc07@gmail.com>
2011-01-03 15:17:19 +0100

    A couple of adjustments to make lilypond happier

--- tolily/main.cpp
+++ tolily/main.cpp
@@ -20,27 +20,27 @@ int main(int argc, char *argv[])
 	outfile.open(QIODevice::WriteOnly);
 
 	QMap<QChar, QString> m;
-	m.insert('a', "a,");
-	m.insert('b', "b,");
-	m.insert('c', "c,");
-	m.insert('d', "d,");
-	m.insert('e', "e,");
-	m.insert('f', "f,");
-	m.insert('g', "g,");
-	m.insert('h', "a");
-	m.insert('i', "b");
-	m.insert('j', "c");
-	m.insert('k', "d");
-	m.insert('l', "e");
-	m.insert('m', "f");
-	m.insert('n', "g");
-	m.insert('o', "a'");
-	m.insert('p', "b'");
-	m.insert('q', "c'");
-	m.insert('r', "d'");
-	m.insert('s', "e'");
-	m.insert('t', "f'");
-	m.insert('u', "g'");
+	m.insert('a', "a");
+	m.insert('b', "b");
+	m.insert('c', "c");
+	m.insert('d', "d");
+	m.insert('e', "e");
+	m.insert('f', "f");
+	m.insert('g', "g");
+	m.insert('h', "a'");
+	m.insert('i', "b'");
+	m.insert('j', "c'");
+	m.insert('k', "d'");
+	m.insert('l', "e'");
+	m.insert('m', "f'");
+	m.insert('n', "g'");
+	m.insert('o', "a''");
+	m.insert('p', "b''");
+	m.insert('q', "c''");
+	m.insert('r', "d''");
+	m.insert('s', "e''");
+	m.insert('t', "f''");
+	m.insert('u', "g''");
 	m.insert('v', "ais");
 	m.insert('w', "bes");
 	m.insert('x', "cis");
@@ -50,11 +50,21 @@ int main(int argc, char *argv[])
 	QString s(infile.readAll());
 	QTextStream os(&outfile);
 
+	os << "\\version \"2.12.3\"\n";
+	os << "{\n";
+	int counter(0);
 	foreach(const QChar& c, s)
 	{
 		if(m.contains(c))
 			os << m.value(c) << " ";
+		if(counter == 8)
+		{
+			os << "\n";
+			counter = -1;
+		}
+		++counter;
 	}
+	os << "\n}\n";
 
 
 //	return a.exec();
